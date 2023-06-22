aa13294784418634da9212d85cfa50ec7e149def
Jafar Hussein <Jafarhussein6@gmail.com>
2023-06-22 20:17:05 +0200

    Fixade test

--- TDDproject/src/test/java/org/MenyTest/MenuTest.java
+++ TDDproject/src/test/java/org/MenyTest/MenuTest.java
@@ -1,21 +1,13 @@
 package MenyTest;
-
-import org.campusmolndal.Database.MongoDbFacade;
 import org.campusmolndal.MainMeny.Menu;
 import org.campusmolndal.InputHanterare.InputHandler;
-import org.campusmolndal.TodoList.Todo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -48,15 +40,16 @@ public class MenuTest {
         when(inputHandler.getIntInput()).thenReturn(0);
 
         // Förväntad utdata
-        String expected = "==== ATT GÖRA-LISTA MENY ====\n" +
-                "1. Skapa Task\n" +
-                "2. Visa Task via ID\n" +
-                "3. Visa alla task\n" +
-                "4. Uppdatera task status\n" +
-                "5. Ta Bort task\n" +
-                "0. Avsluta\n" +
-                "=============================\n" +
-                "Välj ett alternativ: Avslutar...";
+        String expected = """
+                ==== ATT GÖRA-LISTA MENY ====
+                1. Skapa Task
+                2. Visa Task via ID
+                3. Visa alla task
+                4. Uppdatera task status
+                5. Ta Bort task
+                0. Avsluta
+                =============================
+                Välj ett alternativ: Avslutar...""";
 
         // Kör menyn
         menu.run();
@@ -67,9 +60,10 @@ public class MenuTest {
 
         // Jämför antalet rader i förväntad och faktisk utdata
         assertEquals(expectedLines.length, actualLines.length);
+        int lineLength = expectedLines.length;
 
         // Jämför varje rad i förväntad utdata med faktisk utdata
-        for (int i = 0; i < expectedLines.length; i++) {
+        for (int i = 0; i < lineLength; i++) {
             assertEquals(expectedLines[i], actualLines[i]);
         }
     }
