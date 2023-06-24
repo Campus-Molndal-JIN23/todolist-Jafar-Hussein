2bd4ee10f5f80a3aba854773b1663ac383abb246
Jafar Hussein <Jafarhussein6@gmail.com>
2023-06-24 12:15:03 +0200

    Fixade fil struktur och dokumentation

--- TDDproject/src/main/java/org/campusmolndal/Database/MongoDb.java
+++ TDDproject/src/main/java/org/campusmolndal/Database/MongoDb.java
@@ -3,6 +3,7 @@ package org.campusmolndal.Database;
 import com.mongodb.client.*;
 import org.bson.Document;
 import org.campusmolndal.TodoList.Todo;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,84 +15,89 @@ public class MongoDb {
     private static final String connString = "mongodb://localhost:27017";
 
     public MongoDb() {
-        //kopplar upp mot databasen
+        // Establish connection to the database
         MongoClient mongoClient = MongoClients.create(MongoDb.connString);
         MongoDatabase dbName = mongoClient.getDatabase("TodoList");
         collection = dbName.getCollection(MongoDb.collectionName);
     }
 
-    public void createTodoItem(Todo todo) { //skapar ett nytt todo objekt
-        Document document = todo.toDoc();
-        collection.insertOne(document);
+    public void createTodoItem(Todo todo) {
+        // Convert Todo object to JSON
+        JSONObject jsonTodo = new JSONObject();
+        jsonTodo.put("id", todo.getId());
+        jsonTodo.put("text", todo.getText());
+        jsonTodo.put("done", todo.getTaskStatus());
+
+        // Insert JSON document into the collection
+        collection.insertOne(Document.parse(jsonTodo.toString()));
     }
 
-    public Todo getTodoItemById(Integer id) { //hämtar ett todo objekt med ett specifikt id
-        Document filter = new Document("id", id); //skapar ett filter
-        FindIterable<Document> documents = this.collection.find(filter); //hämtar ett todo objekt med ett specifikt id
+    public Todo getTodoItemById(Integer id) {
+        Document filter = new Document("id", id);
+        FindIterable<Document> documents = this.collection.find(filter);
         Document document = documents.first();
 
-        if (null != document) { //om todo objektet finns så skapar vi ett nytt todo objekt
-            //skapar ett nytt todo objekt med id, text och status
-            Integer id1 = document.getInteger("id");
-            String text = document.getString("text");
-            Boolean isDone = document.getBoolean("done");
-            return new Todo( //skapar ett nytt todo objekt
-                    id1,
-                    text,
-                    isDone
-            );
+        if (document != null) {
+            // Convert JSON document to Todo object
+            JSONObject jsonTodo = new JSONObject(document.toJson());
+            Integer id1 = jsonTodo.getInt("id");
+            String text = jsonTodo.getString("text");
+            boolean isDone = jsonTodo.getBoolean("done");
+            return new Todo(id1, text, isDone);
         }
 
         return null;
     }
 
-    public List<Todo> getAllTodoItems() { //hämtar alla todo objekt
-        List<Todo> todos = new ArrayList<>(); //skapar en lista med todo objekt
+    public List<Todo> getAllTodoItems() {
+        List<Todo> todos = new ArrayList<>();
 
-        FindIterable<Document> documents = this.collection.find(); //hämtar alla todo objekt
-        try (MongoCursor<Document> cursor = documents.iterator()) { //loopar igenom alla todo objekt
-            while (cursor.hasNext()) {
-                Document document = cursor.next();
-                Todo todo = Todo.fromDoc(document);
-                todos.add(todo); //lägger till todo objektet i listan
-            }
+        FindIterable<Document> documents = this.collection.find();
+        for (Document document : documents) {
+            // Convert JSON document to Todo object
+            JSONObject jsonTodo = new JSONObject(document.toJson());
+            Integer id = jsonTodo.getInt("id");
+            String text = jsonTodo.getString("text");
+            boolean isDone = jsonTodo.getBoolean("done");
+            Todo todo = new Todo(id, text, isDone);
+            todos.add(todo);
         }
 
-        return todos; //returnerar listan med todo objekt
+        return todos;
     }
 
-
-    public void updateTodoStatus(Integer id, boolean isDone) { // uppdaterar status för ett todo uppdrag
-        Document filter = new Document("id", id); //hittar todo objektet med id
-        //uppdaterar statusen
+    public void updateTodoStatus(Integer id, boolean isDone) {
+        Document filter = new Document("id", id);
         Document update = new Document("$set", new Document("done", isDone));
         collection.updateOne(filter, update);
     }
 
-    public void updateTodoText(Integer id, String newText) { // uppdaterar text för ett todo uppdrag
-        Document filter = new Document("id", id);  //hittar todo objektet med id
+    public void updateTodoText(Integer id, String newText) {
+        Document filter = new Document("id", id);
         Document update = new Document("$set", new Document("text", newText));
-        collection.updateOne(filter, update); //uppdaterar texten
+        collection.updateOne(filter, update);
     }
 
-
-
-    public void deleteTodoItemById(Integer id) { //tar bort ett todo objekt med ett specifikt id
+    public void deleteTodoItemById(Integer id) {
         Document filter = new Document("id", id);
         collection.deleteOne(filter);
     }
 
-    public void viewTodoItemById(Integer id) { //skriver ut ett todo objekt med ett specifikt id
+    public void viewTodoItemById(Integer id) {
         Document filter = new Document("id", id);
         FindIterable<Document> documents = this.collection.find(filter);
         Document document = documents.first();
 
-        if (null != document) {
-            Todo todo = Todo.fromDoc(document);
-            System.out.println(todo); // skriver ut todo objektet
+        if (document != null) {
+            // Convert JSON document to Todo object
+            JSONObject jsonTodo = new JSONObject(document.toJson());
+            Integer todoId = jsonTodo.getInt("id");
+            String text = jsonTodo.getString("text");
+            boolean isDone = jsonTodo.getBoolean("done");
+            Todo todo = new Todo(todoId, text, isDone);
+            System.out.println(todo);
         } else {
             System.out.println("Todo item not found.");
         }
     }
-
 }
